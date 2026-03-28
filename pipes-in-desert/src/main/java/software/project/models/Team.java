f00b6aec7126c8eba4d5ad694d766feb60c135fc
Diego Gomes <diego.gomes@edu.bme.hu>
2026-03-28 03:34:38 +0100

    feat: add method print to Team

--- pipes-in-desert/src/main/java/software/project/models/Team.java
+++ pipes-in-desert/src/main/java/software/project/models/Team.java
@@ -10,12 +10,20 @@ public class Team {
     public List<Player> players;
     public int score;
 
-    public void addPlayer(Player player) {}
-    public void removePlayer(Player player){}
+    public void addPlayer(Player player) {
+        System.out.println("[Team] addPlayer()");
+    }
+
+    public void removePlayer(Player player) {
+        System.out.println("[Team] removePlayer()");
+    }
+
     public int getScore() {
+        System.out.println("[Team] getScore()");
         return score;
     }
     public void addScore(int score) {
+        System.out.printf("[Team] addScore(%d)\n", score);
         this.score += score;
     }
 
