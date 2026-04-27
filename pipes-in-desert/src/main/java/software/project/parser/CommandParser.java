78a3bade545b65d816e90b0488e167cab7f70043
Diego Gomes <diego.gomes@edu.bme.hu>
2026-04-27 13:43:04 +0200

    feat: add Command and CommandParser

---
+++ pipes-in-desert/src/main/java/software/project/parser/CommandParser.java
@@ -0,0 +1,46 @@
+package software.project.parser;
+
+import software.project.core.Game;
+import software.project.parser.commands.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandParser {
+    private final Map<String, Command> commands = new HashMap<>();
+    private final Game game;
+
+    public CommandParser(Game game) {
+        this.game = game;
+        registerCommands();
+    }
+
+    public void registerCommands() {
+        commands.put("NEW_GAME", new NewGameCommand());
+        commands.put("CONFIGURE", null);
+        commands.put("REPAIR", null);
+        commands.put("SABOTAGE", null);
+        commands.put("CONNECT", null);
+        commands.put("DISCONNECT", null);
+        commands.put("PAUSE", null);
+        commands.put("RESUME", null);
+        commands.put("EXIT", null);
+        // Add others, add aliases, etc
+    }
+
+    public void parseAndExecute(String line) {
+        if (line == null || line.trim().isEmpty()) return;
+
+        String[] tokens = line.trim().split("\\s+");
+        String commandKey = tokens[0].toUpperCase();
+        String[] args = Arrays.copyOfRange(tokens, 1, tokens.length);
+
+        Command command = commands.get(commandKey);
+        if (command != null) {
+            command.execute(args);
+        } else {
+            System.out.println("[ERROR] Unknown command: " + commandKey);
+        }
+    }
+}
---
+++ pipes-in-desert/src/main/java/software/project/parser/commands/NewGameCommand.java
@@ -0,0 +1,10 @@
+package software.project.parser.commands;
+
+import software.project.parser.Command;
+
+public class NewGameCommand implements Command {
+    @Override
+    public void execute(String[] args) {
+        // TODO: Implement
+    }
+}
