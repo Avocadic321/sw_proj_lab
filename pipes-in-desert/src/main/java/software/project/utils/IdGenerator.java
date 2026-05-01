274700bc1151bbefd7aa70fa0114d32b6b965c16
Diego Gomes <diego.gomes@edu.bme.hu>
2026-05-01 05:02:10 +0200

    feat: generalize IdGenerator

--- pipes-in-desert/src/main/java/software/project/utils/IdGenerator.java
+++ pipes-in-desert/src/main/java/software/project/utils/IdGenerator.java
@@ -8,17 +8,18 @@ import java.util.Map;
 import java.util.Set;
 
 public final class IdGenerator {
-    private static final Map<Class<? extends Element>, Integer> counters = new HashMap<>();
+    private static final Map<Class<?>, Integer> counters = new HashMap<>();
     private static final Set<String> usedIds = new HashSet<>();
 
     private IdGenerator() {}
 
-    public static String generateId(Class<? extends Element> type) {
+    public static String generateId(Class<?> type) {
         int next = counters.getOrDefault(type, 0) + 1;
         counters.put(type, next);
         String prefix = getPrefix(type);
-        usedIds.add(prefix);
-        return prefix + next;
+        String id = prefix + next;
+        usedIds.add(id);
+        return id;
     }
 
     public static boolean isIdAvailable(String id) {
@@ -34,7 +35,7 @@ public final class IdGenerator {
         usedIds.clear();
     }
 
-    private static String getPrefix(Class<? extends Element> type) {
+    private static String getPrefix(Class<?> type) {
         return switch (type.getSimpleName()) {
             case "Pipe"     -> "PIPE";
             case "Pump"     -> "PUMP";
@@ -42,7 +43,7 @@ public final class IdGenerator {
             case "Spring"   -> "SPRING";
             case "Plumber"  -> "PLUMBER";
             case "Saboteur" -> "SABOTEUR";
-            default         -> "UNDEFINED";
+            default         -> "ENTITY";
         };
     }
 }
