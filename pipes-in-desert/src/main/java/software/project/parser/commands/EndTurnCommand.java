0a336eb48af6a82f89265acd2f557783ce337def
Diego Gomes <diego.gomes@edu.bme.hu>
2026-05-03 13:17:14 +0200

    fix: add missing messages to EndTurnCommand

--- pipes-in-desert/src/main/java/software/project/parser/commands/EndTurnCommand.java
+++ pipes-in-desert/src/main/java/software/project/parser/commands/EndTurnCommand.java
@@ -8,12 +8,15 @@ public class EndTurnCommand implements ICommand {
     @Override
     public void execute(Game game, String[] args) {
         if (game == null) {
+            System.out.println("[ERROR] END_TURN GAME_NOT_INITIALIZED");
             return;
         }
         if (game.getState() != GameState.RUNNING) {
+            System.out.println("[ERROR] END_TURN GAME_NOT_RUNNING");
             return;
         }
         if (args.length != 0) {
+            System.out.println("[ERROR] END_TURN INVALID_ARGUMENTS");
             return;
         }
 
