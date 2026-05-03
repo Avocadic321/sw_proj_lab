438f80eaecbfdda40e7a69adf63c91e7ba6b2e59
Diego Gomes <diego.gomes@edu.bme.hu>
2026-05-03 13:15:08 +0200

    feat: upgrade Debug tools

--- pipes-in-desert/src/main/java/software/project/utils/Debug.java
+++ pipes-in-desert/src/main/java/software/project/utils/Debug.java
@@ -1,19 +1,32 @@
 package software.project.utils;
 
+import java.io.PrintStream;
+
 public final class Debug {
     private Debug() {}
 
-    public static final boolean ENABLED = true;
+    public static final boolean ENABLED = false;
+
+    private static PrintStream output = System.out;
+
+    public static void setOutput(PrintStream stream) {
+        if (stream != null) {
+            output = stream;
+        }
+    }
+
+    public static void useStdout() { output = System.out; }
+    public static void useStderr() { output = System.err; }
 
     public static void log(String fmt, Object... args) {
-        if (Debug.ENABLED) {
-            System.out.printf("[DEBUG] " + fmt + "%n", args);
+        if (ENABLED) {
+            output.printf("[DEBUG] " + fmt + "%n", args);
         }
     }
 
     public static void log(String str) {
-        if (Debug.ENABLED) {
-            System.out.println("[DEBUG] " + str);
+        if (ENABLED) {
+            output.println("[DEBUG] " + str);
         }
     }
 }
