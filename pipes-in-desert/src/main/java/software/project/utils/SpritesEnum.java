cd1ff415d231ef50068c8af192b8c302b89125cc
Artem <monakhov.art321@gmail.com>
2026-05-23 15:18:07 +0200

    feat(utils): SpritesEnum added

---
+++ pipes-in-desert/src/main/java/software/project/utils/SpritesEnum.java
@@ -0,0 +1,23 @@
+package software.project.utils;
+
+public enum SpritesEnum {
+    MENU_BACKGROUND("menu_background", "/ui/menu_background.png"),
+    MENU_PANEL("menu_panel", "/ui/menu_panel.png"),
+    MENU_TITLE("menu_title", "/ui/menu_title_two.png");
+
+    private final String key;
+    private final String path;
+
+    SpritesEnum(String key, String path) {
+        this.key = key;
+        this.path = path;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
