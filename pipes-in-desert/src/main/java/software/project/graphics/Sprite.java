fe00ed42f7dfbb60dd8029ad689520b24155d741
Diego Davidovich Gomes <diego.gomes@edu.bme.hu>
2026-05-23 02:48:31 +0200

    feat: add rotation support for sprites

--- pipes-in-desert/src/main/java/software/project/graphics/Sprite.java
+++ pipes-in-desert/src/main/java/software/project/graphics/Sprite.java
@@ -1,6 +1,7 @@
 package software.project.graphics;
 
 import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 
 public class Sprite {
@@ -20,6 +21,14 @@ public class Sprite {
 
     public void draw(Graphics2D g, int x, int y, int targetWidth, int targetHeight) {
         g.drawImage(image, x, y, targetWidth, targetHeight, null);
+    }
+
+    public void draw(Graphics2D g, int x, int y, double angleDegrees) {
+        AffineTransform original = g.getTransform();
+        g.translate(x + width / 2.0, y + height / 2.0);
+        g.rotate(Math.toRadians(angleDegrees));
+        g.drawImage(image, -width / 2, -height / 2, null);
+        g.setTransform(original);
     }
 
     public BufferedImage getImage() {
