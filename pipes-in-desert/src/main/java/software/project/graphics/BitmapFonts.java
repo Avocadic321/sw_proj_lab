379551eec01843bd9c7c86a748355907798a8b08
Artem <monakhov.art321@gmail.com>
2026-05-25 14:57:58 +0200

    hotfix(BitmapFonts): wrong mapping for fonts

--- pipes-in-desert/src/main/java/software/project/graphics/BitmapFonts.java
+++ pipes-in-desert/src/main/java/software/project/graphics/BitmapFonts.java
@@ -1,8 +1,8 @@
 package software.project.graphics;
 
 public enum BitmapFonts {
-    FONT_MONO(SpriteSheets.FONT_MONO, "abcdefghijklmnopqrstuvwxyz0123456789-+x/=()#@!?.,;'$"),
-    FONT_MAIN(SpriteSheets.FONT_MAIN, "abcdefghijklmnopqrstuvwxyz0123456789");
+    FONT_MONO(SpriteSheets.FONT_MONO, "abcdefghijklmnopqrstuvwxyz1234567890-+x/=()#@!?.,;'$"),
+    FONT_MAIN(SpriteSheets.FONT_MAIN, "abcdefghijklmnopqrstuvwxyz1234567890");
 
     private final SpriteSheets sheet;
     private final String mapping;
